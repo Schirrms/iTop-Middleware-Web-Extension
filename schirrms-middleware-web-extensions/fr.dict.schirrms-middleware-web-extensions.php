64e142474cd059cf7692b6db5d32db6d5c235d2d
Schirrms <schirrms@schirrms.net>
2020-01-10 21:54:41 +0100

    Text improvments (0.1.0.7)

--- schirrms-middleware-web-extensions/fr.dict.schirrms-middleware-web-extensions.php
+++ schirrms-middleware-web-extensions/fr.dict.schirrms-middleware-web-extensions.php
@@ -12,7 +12,7 @@ Dict::Add('FR FR', 'French', 'Français', array(
 	'Class:Zone+' => 'Definit des zones opérationnelles de fonctionnement pour les éléments (Intranet, Internet...)',
 	'Class:Zone/Attribute:name' => 'Nom de la zone',
 	'Class:Zone/Attribute:org_id' => 'Organisation',
-	'Class:Zone/Attribute:org_name' => 'Nom de l`\'organisation',
+	'Class:Zone/Attribute:org_name' => 'Nom de l\'organisation',
 	'Class:Zone/Attribute:comment' => 'Commentaire',
 	'Class:Zone/Attribute:functional_cis_list' => 'CIs dans cette zone',
 	'Class:Zone/Attribute:org_id_friendlyname' => 'Nom complet de l\'organisation',
