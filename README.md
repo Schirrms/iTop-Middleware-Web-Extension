73b2b8e5e192047a2abc0d68a73e116cfe55b568
Schirrms <schirrms@schirrms.net>
2020-01-10 18:58:19 +0100

    Empty delivery (full of empty files) 0.1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # iTop-Middleware-Web-Extension
 Some custom extensions for the Web and Middleware iTop classes - add also a Web Proxy role
+
+## Goal
+
+Probably very 'Custom specific', this extension aims to adds :
+
+To 'Web Server', a notion of 'Project', 'Usage' (Prod, preprod..), and 'Zone' (Internet, Intranet, Extranet...)
+
+Add a specific kind of Web instance : Proxy
+
+To Middleware, adds the sames fields as Web Server
+
+To Middleware instance, add a port number, an URI (ie the path of the middleware in a 'web aspect'), and an URL.
+
+## Status
+
+Empty shell at this time !
+
+## Dependencies
+
+At least iTop in version 2.6.0 ! And, most probably some other custom extensions (for the project and the usage field)
+
+## Installation
+
+As for all my extentions, just download the zip file, and copy the 'schirrms-...' directory in your extensions directory, then rerun the setup as usual.
+
---
+++ schirrms-middleware-web-extensions/module.schirrms-middleware-web-extensions.php
@@ -0,0 +1,54 @@
+<?php
+//
+// iTop module definition file
+//
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'schirrms-middleware-web-extensions/0.1.0',
+	array(
+		// Identification
+		//
+		'label' => 'Some extensions to Web and Middleware',
+		'category' => 'business',
+
+		// Setup
+		//
+		'dependencies' => array(
+			'itop-config-mgmt/2.6.0',
+			'schirrms-project-class/0.1.2',
+			'schirrms-usage-class/1.0.3'
+		),
+		'mandatory' => false,
+		'visible' => true,
+
+		// Components
+		//
+		'datamodel' => array(
+			'model.schirrms-middleware-web-extensions.php'
+		),
+		'webservice' => array(
+			
+		),
+		'data.struct' => array(
+			// add your 'structure' definition XML files here,
+		),
+		'data.sample' => array(
+			// add your sample data XML files here,
+		),
+		
+		// Documentation
+		//
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => '', // hyperlink to more information, if any 
+
+		// Default settings
+		//
+		'settings' => array(
+			// Module specific settings go here, if any
+		),
+	)
+);
+
+
+?>
